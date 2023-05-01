2472623c259ebd289915bb03955688d333105caa
Juwon Park <devprofessionalism@gmail.com>
2023-05-01 17:11:17 +0900

    트렌지션 소개 및 구현

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul>
+  <transition-group name="list" tag="ul">
     <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
       <i class="checkBtn fas fa-check" v-bind:class="{ checkBtnCompleted: todoItem.completed }" v-on:click="toggleComplete(todoItem, index)"></i>
       <span v-bind:class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
@@ -7,7 +7,7 @@
         <i class="fas fa-trash-alt"></i>
       </span>
     </li>
-  </ul>
+  </transition-group>
 </template>
 
 <script>
@@ -56,5 +56,15 @@ li {
 .removeBtn {
   margin-left: auto;
   color: #de4343;
+}
+
+/* transition css */
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
