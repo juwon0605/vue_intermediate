cd4b07024803e9995c3019ec6933fd331987d7b3
Juwon Park <devprofessionalism@gmail.com>
2023-05-01 15:18:10 +0900

    [리팩토링] 할 일 추가 기능

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <todo-header></todo-header>
-    <todo-input></todo-input>
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
     <todo-list v-bind:propsdata="todoItems"></todo-list>
     <todo-footer></todo-footer>
   </div>
@@ -24,6 +24,13 @@ export default {
     return {
       todoItems: [],
     };
+  },
+  methods: {
+    addOneItem: function (todoItem) {
+      var obj = { completed: false, item: todoItem };
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
   },
   created: function () {
     if (localStorage.length > 0) {
