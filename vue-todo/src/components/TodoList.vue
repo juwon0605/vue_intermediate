3e22352e1d5d5253e5e460bccc8a36b767b26d4a
Juwon Park <devprofessionalism@gmail.com>
2023-05-01 14:52:46 +0900

    TodoList 컴포넌트의 할 일 완료 기능 구현

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,11 +1,8 @@
 <template>
   <ul>
-    <li
-      v-for="(todoItem, index) in todoItems"
-      v-bind:key="todoItem"
-      class="shadow"
-    >
-      {{ todoItem }}
+    <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+      <i class="checkBtn fas fa-check" v-bind:class="{ checkBtnCompleted: todoItem.completed }" v-on:click="toggleComplete(todoItem)"></i>
+      <span v-bind:class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
       <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
         <i class="fas fa-trash-alt"></i>
       </span>
@@ -25,11 +22,16 @@ export default {
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index, 1);
     },
+    toggleComplete: function (todoItem) {
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
   },
   created: function () {
     if (localStorage.length > 0) {
       for (var i = 0; i < localStorage.length; ++i) {
-        this.todoItems.push(localStorage.key(i));
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
       }
     }
   },
